3f79a10d4bf58dcde87420178ec4b6eb3180cca1
Virakboth Chau <ccvirakboth12@gmail.com>
2026-05-15 14:48:22 -0400

    Old test.js trial pipeline

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
 console.log('Starting test ...');
-
-setTimeout(() => {
-    console.log('Waiting 3 seconds');
-    console.log('Test complete');
-    
-    // This line tells GitHub Actions: "Everything finished perfectly!"
-    process.exit(0); 
-}, 3000);
+setTimeout(() => console.log('Waiting 3 seconds'), 3000);
+console.log('Test complete');
