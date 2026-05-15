2ca5085919b3d2804ae71a86756dd25a2536372f
Virakboth Chau <ccvirakboth12@gmail.com>
2026-05-15 14:53:21 -0400

    add CI pipeline

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log('Starting test ...');
-setTimeout(() => console.log('Waiting 3 seconds'), 3000);
-console.log('Test complete');
+setTimeout(() => console.log('Waiting 3 seconds'), 3000); //set a timeout of 3 seconds
+console.log('Test complete'); 
